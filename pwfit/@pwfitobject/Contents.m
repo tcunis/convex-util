7be9570bbc4e39fc7c9811783a8060adb5dcd725
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2017-12-07 16:50:24 +0100

    Moved pwpfit toolbox to git submodule.

--- pwfit/@pwfitobject/Contents.m
+++
@@ -1,9 +0,0 @@
-% Class pwfitobject: result of piece-wise fitting.
-%
-%% About
-%
-% * Author:     Torbjoern Cunis
-% * Email:      <mailto:torbjoern.cunis@onera.fr>
-% * Created:    2017-02-23
-%
-%%
--- pwfit/Contents.m
+++
@@ -1,9 +0,0 @@
-%PWFIT Functions and classes for piece-wise data fitting.
-%
-%% About
-%
-% * Author:     Torbjoern Cunis
-% * Email:      <mailto:torbjoern.cunis@onera.fr>
-% * Created:    2017-02-23
-%
-%%
