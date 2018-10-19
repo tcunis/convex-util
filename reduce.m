b9b74d05637117740cfbe837e0ce11416d402168
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2018-10-19 16:57:31 +0200

    Remove nested function calls.

---
+++ reduce.m
@@ -0,0 +1,44 @@
+function varargout = reduce(varargin)
+%REDUCE Remove nested function calls.
+%
+%% Usage and description
+%
+%   [g1,...,gN] = reduce(f1,...,fN, x1,...,xM)
+%
+%% About
+%
+% * Author:     Torbjoern Cunis
+% * Email:      <mailto:torbjoern.cunis@onera.fr>
+% * Created:    2018-10-19
+% * Changed:    2018-10-19
+%
+%%
+
+fhan = cell(1,nargin);
+pvar = cell(1,nargin);
+
+N = 1; M = 1;
+
+for i=1:length(varargin)
+    arg = varargin{i};
+    
+    if isa(arg, 'function_handle')
+        fhan{N} = arg; N = N + 1;
+    else
+        pvar{M} = arg; M = M + 1;
+    end
+end
+
+fhan(N:end) = [];
+pvar(M:end) = [];
+
+psym = cell(size(pvar));
+for i=1:M-1
+    psym{i} = sym(['x' num2str(i) '_'], size(pvar{i}));
+end
+
+fsym = cellfun(@(c) c(psym{:}), fhan, 'UniformOutput', false);
+varargout = cellfun(@(c) matlabFunction(c, 'Vars', psym), fsym, 'UniformOutput', false);
+
+end
+
